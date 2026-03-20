88f5a6a492fa08b60f97d97d1ad75c3889aaeadd
Arthur42-8486 <pozet.arthur42@gmail.com>
2026-03-20 09:09:41 -0400

    création footer

---
+++ Examen-1-react/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+
+function Footer(){
+    return(
+        <footer className='lmj-footer'>
+            <div className='lmj-footer-elem'>
+                Copyright Arthur Pozet
+            </div>
+            <div className='lmj-footer-elem'>
+                Touts droits réservés Tincat. une filiale de Tinder.inc
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
